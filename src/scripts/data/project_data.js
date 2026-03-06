4ed3714bb2c853785ea45663ba743e04198b639c
Stephen <stephen.jszs.gplay@gmail.com>
2026-03-06 18:45:37 +0800

    feat: executed the contact page

--- src/scripts/data/project_data.js
+++ src/scripts/data/project_data.js
@@ -6,8 +6,78 @@ const projects = [
             Made as a Machine Project for the CCAPDEV course AY2526 T2
         `,
         link: "https://github.com/Master9x3r4n/fantastic-dorms",
-        tags: ["Web Application", "Vue"]
+        tags: ["Web Application", "Vue", "Tailwind"]
     },
+    {
+        name: "Squeaky Clean",
+        description: `My first ever created game. Submission for 1TOOGAMEJAM 2025. Made using Python`,
+        link: "https://github.com/Master9x3r4n/squeaky-clean",
+        tags: ["2D Game", "Python", "Pygame"],
+    },
+    {
+        name: "Daily Missions",
+        description: `
+            Simple personal daily missions tracker for everyday tasks. 
+            Inspired by the daily missions system of Honkai: Star Rail
+        `,
+        link: "https://github.com/Master9x3r4n/daily-missions",
+        tags: ["Task List", "Personal", "Python"],
+    },
+    {
+        name: "Infix Calculator",
+        description: `
+            C++ CLI program that accepts a valid infix input expression and outputs the evaluated result.
+        `,
+        link: "https://github.com/Master9x3r4n/cpp-infix-calculator",
+        tags: ["Calculator", "C++"],
+    },
+    {
+        name: "Battleship CLI",
+        description: `Recreation of Battleship in Java CLI.`,
+        link: "https://github.com/Master9x3r4n/battleship-cli",
+        tags: ["CLI", "Game", "Java"],
+    },
+    {
+        name: "Herbie - Catbot",
+        description: `Reinforcement Learning bot that attempts to catch a cat 
+        with different behaviors in a 10x10 grid.`,
+        link: "https://github.com/metatheta/Herbie-Catbot-Edtion",
+        tags: ["Reinforcement Learning", "Q-Learning", "Python"],
+    },
+    {
+        name: "Herbie - Sokobot",
+        description: `Puzzle solving bot that can solve 
+        levels from the puzzle game: Sokoban.`,
+        link: "https://github.com/metatheta/Fantastic-Four-Sokobot",
+        tags: ["Puzzle Solver", "Java"],
+    },
+    {
+        name: "Herbie - Pinoybot",
+        description: `Machine learning algorithm that can predict if a word is English, Filipino, or Other.`,
+        link: "https://github.com/metatheta/Herbie-PinoybotEdition",
+        tags: ["Machine Learning", "Code Switching", "Python"],
+    },
+    {
+        name: "Etch a Sketch",
+        description: `Sample Etch a Sketch Webpage made using JQuery`,
+        link: "https://github.com/Master9x3r4n/etch-a-sketch",
+        tags: ["Web Page", "Jquery"],
+    },
+    {
+        name: "Rock Paper Scissors",
+        description: `Sample Dynamically controlled Rock Paper Scissors Game in JS`,
+        link: "https://github.com/Master9x3r4n/rock-paper-scissors",
+        tags: ["Web Page", "Javascript"],
+    }
 ];
 
 export default projects;
+
+/*
+    {
+        name: "",
+        description: ``,
+        link: "",
+        tags: [],
+    }
+*/
